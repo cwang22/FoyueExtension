611e40d31e676091cac82d273bea3c3509b9a8c2
C.Wang <wcg0304@sina.com>
2014-02-15 23:44:10 +0800

    Add simple error check replace 128px icon pictue

--- js/background.js
+++ js/background.js
@@ -4,43 +4,44 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 chrome.contextMenus.create({
- "title": "佛易",
- "type": "normal",
- "id": "parent",
- "contexts":["selection"]
+	"title" : "佛易",
+	"type" : "normal",
+	"id" : "parent",
+	"contexts" : ["selection"]
 });
 
 chrome.contextMenus.create({
-	"title":"解密",
-	"type": "normal",
-	"id": "Decode",
-	"parentId": "parent",
-	"contexts":["selection"],
-	"onclick": getContent,
+	"title" : "解密",
+	"type" : "normal",
+	"id" : "Decode",
+	"parentId" : "parent",
+	"contexts" : ["selection"],
+	"onclick" : getContent,
 });
 
 chrome.contextMenus.create({
-	"title":"加密",
-	"type": "normal",
-	"id": "Encode",
-	"parentId": "parent",
-	"contexts":["selection"],
-	"onclick": getContent,
+	"title" : "加密",
+	"type" : "normal",
+	"id" : "Encode",
+	"parentId" : "parent",
+	"contexts" : ["selection"],
+	"onclick" : getContent,
 });
 
-function getContent(info, tab){
-  var data = info.selectionText;
-  var action = info.menuItemId;
-  var url = "http://www.keyfc.net/bbs/tools/tudou.aspx";
-  
-  $.post(url,{"data":encodeURIComponent(data), "action":action},function (data){
-			prompt("结果：",$(data).find("Message").text());
+function getContent(info, tab) {
+	var data = info.selectionText;
+	var action = info.menuItemId;
+	var url = "http://www.keyfc.net/bbs/tools/tudou.aspx";
 
-		});
-  
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
+	if (action == "Decode" && data.search(/[(佛曰：)(如是我闻：)]/) == -1) {
+		return;
+	}
 
+	$.post(url, {
+		"data" : encodeURIComponent(data),
+		"action" : action
+	}, function(data) {
+		prompt("结果：", $(data).find("Message").text());
+	});
 }
 
--- js/pop.js
+++ js/pop.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
 	var url = "http://www.keyfc.net/bbs/tools/tudou.aspx";
-	
+
 	$("#encode").click(function() {
 		var content = $("#content").val();
-		$.post(url,{"orignalMsg":encodeURIComponent(content), "action":"Encode"},function (data){
+		$.post(url, {
+			"orignalMsg" : encodeURIComponent(content),
+			"action" : "Encode"
+		}, function(data) {
 			$("#content").val($(data).find("Message").text());
 		});
 	});
-	
+
 	$("#decode").click(function() {
 		var content = $("#content").val();
-		$.post(url,{"orignalMsg":encodeURIComponent(content), "action":"Decode"},function (data){
-			$("#content").val($(data).find("Message").text());
-		});
+		if (content.search(/[(佛曰：)(如是我闻：)]/) != -1) {
+			$.post(url, {
+				"orignalMsg" : encodeURIComponent(content),
+				"action" : "Decode"
+			}, function(data) {
+				$("#content").val($(data).find("Message").text());
+			});
+		}
 	});
-}); 
+});
+
